b5506c8da3b17c37b5bb5aec7e75ebcbe13d937c
cung.nguyen <cung.nguyen@formos.com>
2023-04-14 10:57:03 +0700

    Remove code front end

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -5,8 +5,6 @@ import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
 
 @Component({
   selector: 'jhi-home',
@@ -15,16 +13,6 @@ import { getMessaging, getToken } from 'firebase/messaging';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
-  firebaseConfig = {
-    apiKey: 'AIzaSyAgWfa6LJGBW3PfBEcpb1M-8WTKP3wFiK4',
-    authDomain: 'fluted-depth-362515.firebaseapp.com',
-    projectId: 'fluted-depth-362515',
-    storageBucket: 'fluted-depth-362515.appspot.com',
-    messagingSenderId: '147801621083',
-    appId: '1:147801621083:web:50f9085327535af588357e',
-  };
-  app: any;
-  messaging: any;
 
   private readonly destroy$ = new Subject<void>();
 
@@ -35,19 +23,6 @@ export class HomeComponent implements OnInit, OnDestroy {
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
-
-    this.app = initializeApp(this.firebaseConfig);
-    this.messaging = getMessaging(this.app);
-    // this.getToken(this.messaging)
-    getToken(this.messaging, { vapidKey: 'BFSzaLNc9oqaXU6dgCFYKC3E-S_o7fnRc_I97i93r3HVU9Ven-6_1D65IugkRQAUxKTJz9nJXpLtZ9lKk6UbmOU' }).then(
-      currentToken => {
-        if (currentToken) {
-          console.log('current token', currentToken);
-        } else {
-          console.log('Can not get token');
-        }
-      }
-    );
   }
 
   login(): void {
